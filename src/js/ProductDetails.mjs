1a4ba225b24c803c407a0c42096d7890998e6e35
benjamincouto <ben.couto@live.com>
2024-11-13 19:55:19 -0300

    npm build and updated some double quotes

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -28,12 +28,12 @@ export default class ProductDetails {
     // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
     this.product = await this.dataSource.findProductById(this.productId);
     // once we have the product details we can render out the HTML
-    this.renderProductDetails("main");
+    this.renderProductDetails('main');
     // once the HTML is rendered we can add a listener to Add to Cart button
     // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
     document
-      .getElementById("addToCart")
-      .addEventListener("click", this.addToCart.bind(this));
+      .getElementById('addToCart')
+      .addEventListener('click', this.addToCart.bind(this));
   }
   addToCart() {
     let cartItems = getLocalStorage('so-cart');
@@ -50,7 +50,7 @@ export default class ProductDetails {
   renderProductDetails(selector) {
     const element = document.querySelector(selector);
     element.insertAdjacentHTML(
-      "afterBegin",
+      'afterBegin',
       productDetailsTemplate(this.product)
     );
   }
