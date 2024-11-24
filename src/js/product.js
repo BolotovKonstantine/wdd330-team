db5e5627bfa1d6222d25d439060ffdf337788e9f
gino004 <gino2137@hotmail.com>
2024-11-23 21:20:45 -0500

    Work

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,16 @@
 import { setLocalStorage } from "./utils.mjs";
 import { getLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getParam } from './utils.mjs';
 import ProductData from "./ProductData.mjs";
+import ProductDetails from './ProductDetails.mjs';
+
 
 const dataSource = new ProductData("tents");
+const productId = getParam('product');
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
   let cartItems = getLocalStorage("so-cart");
