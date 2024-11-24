5b1d16fe19243be3b34de0ac6050b7c701c284cf
Sogiefa-12 <131720010+Sogiefa-12@users.noreply.github.com>
2024-11-24 16:07:20 +0100

    commit

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -53,12 +53,22 @@ async function loadTemplate(path) {
   return template;
 }
 
+// function to dynamically load the header and footer into a page
 export async function loadHeaderFooter() {
-  const headerTemplate = await loadTemplate('../partials/header.html');
-  const headerElement = document.querySelector('#main-header');
-  const footerTemplate = await loadTemplate('../partials/footer.html');
-  const footerElement = document.querySelector('#main-footer');
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
 
   renderWithTemplate(headerTemplate, headerElement);
   renderWithTemplate(footerTemplate, footerElement);
+}
+
+// set a listener for both touchend and click
+export function setClick(selector, callback) {
+  qs(selector).addEventListener("touchend", (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener("click", callback);
 }
